e0ecaa6d1dc1d21605dc701cbe809461bd5aeff3
casadoj <jesus.casado-rodriguez@ec.europa.eu>
2024-09-10 14:27:22 +0200

    Update coarser_grid.py

--- src/lisfloodpreprocessing/coarser_grid.py
+++ src/lisfloodpreprocessing/coarser_grid.py
@@ -24,11 +24,12 @@ logger = logging.getLogger(__name__)
 
 def coordinates_coarse(
     cfg: Config,
-    points: pd.DataFrame,
+    points_fine: Union[pd.DataFrame, gpd.GeoDataFrame],
+    polygons_fine: gpd.GeoDataFrame,
     ldd_coarse: xr.DataArray,
     upstream_coarse: xr.DataArray,
     reservoirs: bool = False,
-    save: bool = True
+    save: bool = False
 ) -> Optional[gpd.GeoDataFrame]:
     """
     Transforms point coordinates from a high-resolution grid to a corresponding location in a coarser grid, aiming to match the shape of the catchment area derived from the high-resolution map. It updates the station coordinates and exports the catchment areas as shapefiles in the coarser grid.
@@ -39,8 +40,10 @@ def coordinates_coarse(
     -----------
     cfg: Config
         Configuration object containing file paths and parameters specified in the configuration file.
-    points: pandas.DataFrame
-        DataFrame containing station coordinates and upstream areas in the finer grid. It's the result of `coarse_grid.coarse_grid()`
+    points_fine: pandas.DataFrame or geopandas.GeoDataFrame
+        A table with updated station coordinates and upstream areas in the finer grid. It's the result of coarse_grid.coarse_grid()`
+    polygons_fine: geopandas.GeoDataFrame
+        A table with the catchment polygons in the finer grid. It's the result of `coarse_grid.coarse_grid()`
     ldd_coarse: xarray.DataArray
         Map of local drainaige directions in the coarse grid
     upstream_coarse: xarray.DataArray
@@ -53,8 +56,12 @@ def coordinates_coarse(
     Returns:
     --------
     points: geopandas.GeoDataFrame
-        A pandas DataFrame with updated station coordinates and upstream areas in the coarser grid.
+        A table with updated station coordinates and upstream areas in the coarser grid.
+    polygons: geopandas.GeoDataFrame
+        A table with the catchment polygons in the coarser grid.
     """
+    
+    points = points_fine.copy()
     
     # create river network
     fdir_coarse = pyflwdir.from_array(ldd_coarse.data,
@@ -63,25 +70,20 @@ def coordinates_coarse(
                                       check_ftype=False,
                                       latlon=True)
 
-    # boundaries of the input maps
+    # boundaries and resolution of the input maps
     lon_min, lat_min, lon_max, lat_max = np.round(ldd_coarse.rio.bounds(), 6)
-
-    # resolution of the input maps
     cellsize = np.round(np.mean(np.diff(ldd_coarse.x)), 6) # degrees
-    cellsize_arcmin = int(np.round(cellsize * 60, 0)) # arcmin
-    # cfg.COARSE_RESOLUTION = f'{cellsize_arcmin}min'
-    coarse_resolution = f'{cellsize_arcmin}min'
 
     # extract resolution of the finer grid from 'points'
     cols_fine = [f'{col}_{cfg.FINE_RESOLUTION}' for col in ['area', 'lat', 'lon']]
 
     # add new columns to 'points'
-    cols_coarse = [f'{col}_{coarse_resolution}' for col in ['area', 'lat', 'lon']]
+    cols_coarse = [f'{col}_{cfg.COARSE_RESOLUTION}' for col in ['area', 'lat', 'lon']]
     points[cols_coarse] = np.nan
-
 
     # search range of 5x5 array -> this is where the best point can be found in the coarse grid
     rangexy = np.linspace(-2, 2, 5) * cellsize # arcmin
+    polygons_coarse = []
     for ID, attrs in tqdm(points.iterrows(), total=points.shape[0], desc='points'):
 
         # real upstream area
@@ -92,18 +94,6 @@ def coordinates_coarse(
 
         if (area_ref < cfg.MIN_AREA) or (area_fine < cfg.MIN_AREA):
             logger.warning(f'The catchment area of station {ID} is smaller than the minimum of {cfg.MIN_AREA} km2')
-            continue
-
-        # import shapefile of catchment polygon
-        shapefile = cfg.OUTPUT_FOLDER_FINE / f'{ID}.shp'
-        try:
-            basin_fine = gpd.read_file(shapefile)
-            logger.info(f'Catchment polygon correctly read: {shapefile}')
-        except OSError as e:
-            logger.error(f'Error reading {shapefile}: {e}')
-            continue
-        except Exception as e:  # This will catch other exceptions that might occur.
-            logger.error(f'An unexpected error occurred while reading {shapefile}: {e}')
             continue
 
         # find ratio
@@ -118,8 +108,8 @@ def coordinates_coarse(
                                           crs=ldd_coarse.rio.crs)
 
                 # calculate union and intersection of shapes
-                intersection = gpd.overlay(basin_fine, basin, how='intersection')
-                union = gpd.overlay(basin_fine, basin, how='union')
+                intersection = gpd.overlay(polygons_fine.loc[[ID]], basin, how='intersection')
+                union = gpd.overlay(polygons_fine.loc[[ID]], basin, how='union')
                 inter_vs_union.append(intersection.area.sum() / union.area.sum())
 
                 # get upstream area (km2) of coarse grid (LISFLOOD)
@@ -169,28 +159,33 @@ def coordinates_coarse(
         basin_coarse.set_index('ID', inplace=True)
         basin_coarse[cols_fine] = area_fine, lat_fine, lon_fine
         basin_coarse[cols_coarse] = area_coarse, lat_coarse, lon_coarse
-
-        # export shapefile
-        output_shp = cfg.OUTPUT_FOLDER_COARSE / f'{ID}.shp'
-        basin_coarse.to_file(output_shp)
-        logger.info(f'Catchment {ID} exported as shapefile: {output_shp}')
-
+        
+        # save polygon
+        polygons_coarse.append(basin_coarse)
+        
         # move the result one pixel downstream, in case of reservoir
         if reservoirs:
             lat_coarse, lon_coarse = downstream_pixel(lat_coarse, lon_coarse, ldd_coarse)
             
         # update new columns in 'points'
         points.loc[ID, cols_coarse] = [int(area_coarse), round(lat_coarse, 6), round(lon_coarse, 6)]
+        
+    # concatenate polygons shapefile
+    polygons_coarse = pd.concat(polygons_coarse)
     
-    # convert to geopandas
-    geometry = [Point(xy) for xy in zip(points[f'lon_{coarse_resolution}'], points[f'lat_{coarse_resolution}'])]
+    # convert points to geopandas
+    geometry = [Point(xy) for xy in zip(points[f'lon_{cfg.COARSE_RESOLUTION}'], points[f'lat_{cfg.COARSE_RESOLUTION}'])]
     points = gpd.GeoDataFrame(points, geometry=geometry, crs=4326)
-    
-    # return (save)
     points.sort_index(axis=1, inplace=True)
+    
     if save is True:
-        shp_file = cfg.OUTPUT_FOLDER_COARSE / f'{cfg.POINTS.stem}_{coarse_resolution}.shp'
-        points.to_file(shp_file)
-        logger.info(f'The updated points table in the coarser grid has been exported to: {shp_file}')
+        # polygons
+        polygon_shp = cfg.OUTPUT_FOLDER / f'catchments_{cfg.COARSE_RESOLUTION}.shp'
+        polygons_coarse.to_file(polygon_shp)
+        logger.info(f'Catchments in the coarser grid have been exported to: {polygon_shp}')
+        # points
+        point_shp = cfg.OUTPUT_FOLDER / f'{cfg.POINTS.stem}_{cfg.COARSE_RESOLUTION}.shp'
+        points.to_file(point_shp)
+        logger.info(f'The updated points table in the coarser grid has been exported to: {point_shp}')
 
-    return points
+    return points, polygons_coarse
